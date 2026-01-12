60847ff2f79adfe25c3068083cec228678fcac92
Jo Andersson Stokke <jo.stokke.andersson@gmail.com>
2026-01-12 11:10:55 +0100

    added pyright to upsert script and made changed the script to list what to include instead of exclude

--- scripts/upsert-dotfiles.sh
+++ scripts/upsert-dotfiles.sh
@@ -1,7 +1,12 @@
-
 #!/usr/bin/env bash
 
 REPO_URL="https://github.com/Antire-AS/antire-public-dotfiles.git"
+
+INCLUDE_ITEMS=(
+  ".gitignore"
+  ".ruff.toml"
+  "pyrightconfig.json"
+)
 
 tmp=$(mktemp -d)
 overwrite_all=false
@@ -10,15 +15,9 @@ git clone --depth=1 "$REPO_URL" "$tmp"
 
 shopt -s nullglob
 
-for item in "$tmp"/* "$tmp"/.*; do
-  name=$(basename "$item")
 
-  [ "$name" = "." ] && continue
-  [ "$name" = ".." ] && continue
-  [ "$name" = ".git" ] && continue
-  [ "$name" = ".github" ] && continue
-  [ "$name" = ".releaserc.json" ] && continue
-  [ "$name" = "README" ] && continue
+for name in "${INCLUDE_ITEMS[@]}"; do
+  item="$tmp/$name"
 
   if [ -e "./$name" ] && [ "$overwrite_all" = false ]; then
     while true; do
