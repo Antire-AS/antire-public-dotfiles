3bf95e83cac4fecee971891725eb4a2632079d0e
Jo Andersson Stokke <jo.stokke.andersson@gmail.com>
2026-02-04 12:51:19 +0100

    added to script

--- scripts/upsert-dotfiles.sh
+++ scripts/upsert-dotfiles.sh
@@ -6,6 +6,8 @@ INCLUDE_ITEMS=(
   ".gitignore"
   ".ruff.toml"
   "pyrightconfig.json"
+  "pytest.ini"
+  ".coveragerc"
 )
 
 tmp=$(mktemp -d)
@@ -15,33 +17,32 @@ git clone --depth=1 "$REPO_URL" "$tmp"
 
 shopt -s nullglob
 
-
 for name in "${INCLUDE_ITEMS[@]}"; do
   item="$tmp/$name"
 
   if [ -e "./$name" ] && [ "$overwrite_all" = false ]; then
     while true; do
       read -rp \
-"File '$name' already exists.
+        "File '$name' already exists.
 [y] overwrite this file
 [n] skip this file
 [a] overwrite this file and ALL remaining dotfiles from antire-public-dotfiles
 Choose [y/n/a]: " answer
 
       case "$answer" in
-        y|Y)
-          break
-          ;;
-        n|N)
-          continue 2
-          ;;
-        a|A)
-          overwrite_all=true
-          break
-          ;;
-        *)
-          echo "Please enter y, n, or a."
-          ;;
+      y | Y)
+        break
+        ;;
+      n | N)
+        continue 2
+        ;;
+      a | A)
+        overwrite_all=true
+        break
+        ;;
+      *)
+        echo "Please enter y, n, or a."
+        ;;
       esac
     done
   fi
@@ -52,5 +53,3 @@ done
 rm -rf "$tmp"
 
 echo "Done."
-
-
